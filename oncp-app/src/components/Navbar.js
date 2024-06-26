d5e4c9518a2ab839e65ee17359c8ac1fae329e72
unknown <tatlidilonur0@gmail.com>
2024-06-26 00:36:22 -0400

    ddmenu fixed

--- oncp-app/src/components/Navbar.js
+++ oncp-app/src/components/Navbar.js
@@ -23,7 +23,7 @@ const CustomNavbar = () => {
         <div className="container">
           <Link className="navbar-brand" to="/">ONCP</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"><i className="fas fa-bars"></i></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
